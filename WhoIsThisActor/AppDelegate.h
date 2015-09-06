eb67f5c2f1226f3854fed193dd3792227a91aad3
HuyLafa <anhhuy0706@gmail.com>
2015-09-06 02:18:25 -0400

    Initial Commit

---
+++ WhoIsThisActor/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  WhoIsThisActor
+//
+//  Created by Nguyen Huy on 9/6/15.
+//  Copyright (c) 2015 Nguyen Huy. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ WhoIsThisActor/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  WhoIsThisActor
+//
+//  Created by Nguyen Huy on 9/6/15.
+//  Copyright (c) 2015 Nguyen Huy. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
